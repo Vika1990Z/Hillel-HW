e9d93ed51b07d3f54f7a8d1760c222c09aee57ab
Vika1990Z <53933994+Vika1990Z@users.noreply.github.com>
2022-09-29 19:50:26 +0300

    add 13 home work

---
+++ HomeWork_13.js
@@ -0,0 +1,19 @@
+// Напишите функцию limitStr(str, n, symb), которая обрезает строку, если она длиннее указанного количества символов n. Усеченная строка должна заканчиваться троеточием «...» (если не задан параметр symb) или заданным символом symb.
+
+function limitStr(str, n, symb="...") {
+    let resultStr = "";
+    if (str.length > n) resultStr = str.substring(0,n)+symb;
+    else resultStr=str;
+    console.log(`RESULT: ${resultStr}`);
+    return resultStr;
+}
+
+// tests from task's condition:
+limitStr('Каждый охотник желает знать.'); // RESULT: Каждый охотник желает знать.
+limitStr('Каждый охотник желает знать.', 14); // RESULT: Каждый охотник...
+limitStr('Каждый охотник желает знать.', 21, '!'); // RESULT: Каждый охотник желает!
+
+// own test 
+limitStr('Каждый охотник желает знать.', 50, '!'); //  RESULT: Каждый охотник желает знать. 
+limitStr('Каждый охотник желает знать.', 0, '!'); //  !
+limitStr('Каждый охотник желает знать.', 22, "!"); // K!
