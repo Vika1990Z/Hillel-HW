6dbe0f1c9643f105318b209ff511d71757981ff8
Vika1990Z <53933994+Vika1990Z@users.noreply.github.com>
2022-10-10 22:39:55 +0300

    add 38 home work

--- main.js
+++ main.js
@@ -1,90 +1,51 @@
+function httpGet (object) {
+  return new Promise ((resolve, reject) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open(object.method, object.url);
+    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    xhr.onload = () => {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve(xhr.response);
+      } else {
+        reject(xhr.status);
+      }
+    }
+    xhr.onerror = () => {
+      reject(xhr.status);
+    }
 
-let url = 'https://jsonplaceholder.typicode.com/posts'
-let body = document.body;
-let emptyBlock = body.querySelector('.empty');
-let btnAddPost = body.querySelector('.add');
-let cardsHolder = body.querySelector('.cards');
-let inputs = body.querySelectorAll('.input');
-let submitForm = body.querySelector('.submit');
-let form = body.querySelector('form');
-let btnCloseForm = body.querySelector('.closebtn')
-let previousBgNumber;
+    xhr.send(object.body);
 
-btnCloseForm.addEventListener('click', (e)=> {
-  e.preventDefault();
-  closeForm();
-})
-
-btnAddPost.addEventListener('click', function(){
-  form.style.height = "100%";
-  // prevent page refresh on enter
-  inputs.forEach(input => {
-    input.addEventListener('keypress', (e) => {
-      if (e.key === 'Enter') e.preventDefault();
-    })
   })
-})
   
-submitForm.addEventListener('click', (event) => {
-  event.preventDefault();
-  inputValues = Array.from(inputs).map(input => input.value);
-
-  if (inputValues.includes('')) {
-    alert ("Not full information was added");
-    inputs.forEach(input => {
-      if (input.value === "") input.classList.add('notvalid');
-      input.addEventListener('keyup', () => input.classList.remove('notvalid'))
-    })
-    return
-  }
-
-  fetch(url, {
-    method: 'POST',
-    body: JSON.stringify({
-      title: inputs[0].value,
-      body: inputs[1].value,
-      userId: inputs[2].value,
-      }),
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-      },
-    })
-  .then((response) => response.json())
-  .then((data) => {
-    renderCard(data);
-    closeForm();
-    removeEmptyBlock();
-  });
-})
-
-
-//HANDLERS:
-
-// close form and clean inputs
-let closeForm = () => {
-  form.style.height = "0";
-
-  inputs.forEach(input => {
-    input.value= "";
-    input.classList.remove('notvalid')
-  })
 }
 
-// render one card
+httpGet({
+  url: "https://jsonplaceholder.typicode.com/posts",
+  method: 'GET',
+})
+.then(data => {
+    let posts = JSON.parse(data);
+    console.log('method: GET');
+    console.log('posts:', posts);
+    posts.forEach(post => {
+      renderCard(post);
+    });
+})
+.catch(error => {
+    console.log("error:", error);
+});
+
+
 let renderCard = data => {
-  console.log(data);
+  let body = document.body;
+  let cardsHolder = body.querySelector('.cards');
+
   let card = document.createElement('div');
   card.classList.add('card');
 
-  if (cardsHolder.lastChild === null) {
-    previousBgNumber = 0;
-  } else {
-    previousBgNumber = +cardsHolder.lastChild.firstChild.textContent;
-  }
-  let bgNumber = previousBgNumber+1;
-
   let cardBackground = document.createElement('background-text');
-  cardBackground.textContent = bgNumber;
+  cardBackground.textContent = data.id;
 
   let cardHeader = document.createElement('h3');
   cardHeader.textContent = data.title;
@@ -93,43 +54,48 @@ let renderCard = data => {
   cardBody.innerHTML = `<h4>Post for User ${data.userId}</h4>
   <p> ${data.body}</p>
   `
-
-  let deleteButton = document.createElement('button');
-  deleteButton.textContent = 'delete';   
-  deleteButton.classList.add('delete');
-
+  
   card.append(cardBackground);
-  card.append(deleteButton);
   card.append(cardHeader);
   card.append(cardBody);
   cardsHolder.append(card);
-
-  if (cardsHolder.childElementCount === 1 ) card.classList.add('oneCard');
-  else {cardsHolder.firstChild.classList.remove('oneCard')};
-
-  addFunctionality(card, deleteButton);
-}
   
-// remove block "no added posts"
-let removeEmptyBlock = () => {
-
-    console.log('card on the page', cardsHolder.childElementCount);
-    if (cardsHolder.childElementCount > 0) {
-      emptyBlock.style.display = "none";
-    }
 }
 
-// add functionality to card 
-let addFunctionality = (card, deleteButton) => {
-  card.addEventListener ('click', () => {
-    card.classList.toggle('active');
-  })
+// httpGet({
+//   url: "https://jsonplaceholder.typicode.com/posts",
+//   method: 'POST',
+//   body: JSON.stringify({
+//     title: "Hello",
+//     body: "hello from Vika",
+//     userId: 1,
+//   })
+// })
+// .then(data => {
+//     console.log('method: POST');
+//     let posts = JSON.parse(data);
+//     console.log('post:', posts);
+//     if (!Array.isArray(posts)) {
+//       renderCard(posts);
+//     }
 
-  deleteButton.addEventListener('click', () => {
-    card.remove();
-    if (cardsHolder.childElementCount === 0 ) emptyBlock.style.display = 'block';
-    if (cardsHolder.childElementCount === 1 ) cardsHolder.firstChild.classList.add('oneCard');
-  })
+// })
+// .catch(error => {
+//     console.log("error:", error);
+// });
 
-}
+    
 
+
+// httpGet({
+//   url: "https://jsonplaceholder.typicode.com/posts/100",
+//   method: 'DELETE',
+// })
+
+// .then(() => {
+//     console.log('method: DELETE');
+// })
+// .catch(error => {
+//     console.log("error:", error);
+// });    
+    
