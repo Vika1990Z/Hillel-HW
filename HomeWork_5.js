289ba141f01f421cc2db66ac8999f4e5dd03ac24
Vika1990Z <53933994+Vika1990Z@users.noreply.github.com>
2022-10-17 13:11:12 +0300

    add updated task

--- HomeWork_5.js
+++ HomeWork_5.js
@@ -1,14 +1,25 @@
+//___________________task form HILLEL________________________
 // Вывести на страницу в одну строку через запятую числа от 10 до 20 и наоборот.
 
+// for (let i=10; i<=20; i++) {
+//     document.write(i+",");
+// }
+
+// for (let j=20; j>=10; j--) {
+//     if (j == 10) {
+//         document.write(j);
+//     } else {
+//         document.write(j+",");
+//     }        
+// } 
+
+
+
+//___________________updated task________________________
+// зробити використовуючи лише один цикл
+
+let str = "";
 for (let i=10; i<=20; i++) {
-    document.write(i+",");
+    (i<20) ? str += i+"," : str += i;
 }
-
-for (let j=20; j>=10; j--) {
-    if (j == 10) {
-        document.write(j);
-    } else {
-        document.write(j+",");
-    }        
-
-} 
+console.log(`${str},${str.split(",").reverse().join(",")}`);
