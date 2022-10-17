c8890ee71a7e6a5bde52dc6a5a904c1d9b2eda12
Vika1990Z <53933994+Vika1990Z@users.noreply.github.com>
2022-10-17 13:13:53 +0300

    add updated task

--- HomeWork_9.js
+++ HomeWork_9.js
@@ -1,16 +1,31 @@
-// Вводится строка произвольного содержания (буквы, числа, символы). Найти все числа в строке. Результат число. Например, если дана строка 23Gjf4uf21, результат 23421
+//___________________task form HILLEL________________________
+// Вводится строка произвольного содержания (буквы, числа, символы). Найти все
+// числа в строке. Результат число. 
+//Например, если дана строка 23Gjf4uf21, результат 23421
 
-let myStr = prompt ('Please, enter some sequence of characters');
-while (myStr === null || myStr === ''){
-    myStr = prompt('Please, enter some sequence of characters');
+// let myStr = prompt ('Please, enter some sequence of characters');
+// while (myStr === null || myStr === ''){
+//     myStr = prompt('Please, enter some sequence of characters');
+// }
+// console.log(`INFO: entered string: "${myStr}"`);
+
+// let resultStr = "";
+// for (let i = 0; i<myStr.length; i++) {
+//     if (!isNaN(Number(myStr[i])) && myStr[i]!=" " ) {
+//         resultStr += myStr[i];
+//     }
+// }
+
+// console.log(`RESUlT: "${resultStr}"`);
+
+
+
+
+//___________________updated task________________________
+// переписати невикористовуючи методи Number() isNaN() та перевірку на пробіл
+
+function filterNumbers(str) {
+    return str.replace(/\D/g,'');
 }
-console.log(`INFO: entered string: "${myStr}"`);
 
-let resultStr = "";
-for (let i = 0; i<myStr.length; i++) {
-    if (!isNaN(Number(myStr[i])) && myStr[i]!=" " ) {
-        resultStr += myStr[i];
-    }
-}
-
-console.log(`RESUlT: "${resultStr}"`);
+console.log(filterNumbers('jsahdfg2498576 )))rhg'));
