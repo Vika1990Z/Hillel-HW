223faa6e4b018d879b0ea6ff5b7a91b672c92109
Vika1990Z <53933994+Vika1990Z@users.noreply.github.com>
2022-10-17 13:12:57 +0300

    add updated task

--- HomeWork_8.js
+++ HomeWork_8.js
@@ -1,31 +1,63 @@
+//___________________task form HILLEL________________________
 // Найти в строке указанную подстроку и заменить ее на новую. 
 // Строку, ее подстроку для замены и новую подстроку вводит пользователь. 
 // indexOf, substring, substr
 
-let str = prompt('Please, enter initial string');
-while (str === null || str === ''){
-    str = prompt('Please, enter initial string');
+// let str = prompt('Please, enter initial string');
+// while (str === null || str === ''){
+//     str = prompt('Please, enter initial string');
+// }
+// console.log(`INFO: initial string: "${str}"`);
+
+
+// let subStr = prompt('Please, enter substring, that you want to change');
+// while (subStr === null || subStr === '' || str.indexOf(subStr,0) < 0){
+//     if (str.indexOf(subStr,0) < 0){
+//         console.log (`ERR: entered subString: "${subStr}" not a part of initial string: "${str}"`);
+//         subStr = prompt('Please, enter substring, that you want to change');
+//     }
+//     subStr = prompt('Please, enter substring, that you want to change');   
+// }
+// console.log(`INFO: entered subString: "${subStr}"`);
+
+
+// let newSubStr = prompt('Please, enter new substring');
+// while (newSubStr === null || newSubStr === ''){
+//     newSubStr = prompt('Please, enter new substring');
+// }
+// console.log(`INFO: entered new subString: "${newSubStr}"`);
+
+
+// let resultStr = str.replace(subStr, newSubStr);
+// console.log(`RESUlT: "${resultStr}"`)
+
+
+
+
+
+//___________________updated task________________________
+// замінити не лише перше знайдене входження підрядку у рядку, а усі що є. 
+
+function replaceStr (str, subStr, newSubStr) {
+    return str.replaceAll(subStr, newSubStr);
 }
-console.log(`INFO: initial string: "${str}"`);
 
+let str = `Betty Botter bought some butter
+But, said she, the butter's bitter.
+If I put it in my batter,
+it will make my batter bitter.
+But a bit of better butter
+will make my bitter batter better.
+So she bought some better butter,
+better than the bitter butter,
+put it in her bitter batter,
+made her bitter batter better.
+So it was better
+Betty Botter bought some better butte
+`;
 
-let subStr = prompt('Please, enter substring, that you want to change');
-while (subStr === null || subStr === '' || str.indexOf(subStr,0) < 0){
-    if (str.indexOf(subStr,0) < 0){
-        console.log (`ERR: entered subString: "${subStr}" not a part of initial string: "${str}"`);
-        subStr = prompt('Please, enter substring, that you want to change');
-    }
-    subStr = prompt('Please, enter substring, that you want to change');   
-}
-console.log(`INFO: entered subString: "${subStr}"`);
+let subStr = 'tter';
 
+let newSubStr = 'hello';
 
-let newSubStr = prompt('Please, enter new substring');
-while (newSubStr === null || newSubStr === ''){
-    newSubStr = prompt('Please, enter new substring');
-}
-console.log(`INFO: entered new subString: "${newSubStr}"`);
-
-
-let resultStr = str.replace(subStr, newSubStr);
-console.log(`RESUlT: "${resultStr}"`)
+console.log(replaceStr (str, subStr, newSubStr));
