0b25ee1d331730702cf480d992a344db7fed6fcb
Vika1990Z <53933994+Vika1990Z@users.noreply.github.com>
2022-10-18 12:42:26 +0300

    add 40 home work

--- main.js
+++ main.js
@@ -1,17 +1,58 @@
-const sumAllNumbers = (...numbers) => {
-    let result = 0;
-    
-    for (element of numbers) {
-        if (!Number.isInteger(element)) return "Error! You can use only numbers.";
-        result += element
-    };
-    return result;
+const userData = {
+  "id": 1,
+  "name": "Leanne Graham",
+  "username": "Bret",
+  "email": "Sincere@april.biz",
+  "address": {
+    "street": "Kulas Light",
+    "suite": "Apt. 556",
+    "city": "Gwenborough",
+    "zipcode": "92998-3874",
+    "geo": {
+      "lat": "-37.3159",
+      "lng": "81.1496"
+    }
+  },
+  "phone": "1-770-736-8031 x56442",
+  "website": "hildegard.org",
+  "company": {
+    "name": "Romaguera-Crona",
+    "catchPhrase": "Multi-layered client-server neural-net",
+    "bs": "harness real-time e-markets"
+  }
+};
+
+const { 
+  id, 
+  name, 
+  username, 
+  email, 
+  address: {street, suite, city, zipcode, geo: {lat, lng}},
+  phone,
+  website,
+  company: {name: companyName, catchPhrase, bs}
+} = userData;
+
+const main = document.querySelector('.cards');
+
+const renderUser = () => {
+  let card = document.createElement('div');
+  card.classList.add('card');
+
+  card.innerHTML = `
+  <background-text>${ id }</background-text>
+  <h3>${ name }</h3>
+  <p><span>Username: </span> ${ username }</p> 
+  <p><span>Email: </span> ${ email }</p> 
+  <p><span>Phone: </span> ${ phone }</p> 
+  <p><span>Website: </span> ${ website }</p> 
+  <p><span>Company: </span> ${ companyName }</p> 
+  <p><span>Catch Phrase: </span> ${ catchPhrase }</p> 
+  <p><span>Address: </span> ${ street }, ${ suite }, ${ city }, ${ zipcode }</p> 
+  <p><span>Geo: </span> (${ lat }, ${ lng })</p> 
+  `
+  main.append(card);
 }
 
-console.log(sumAllNumbers(1)) // 1
-console.log(sumAllNumbers(1, 2, 3)) // 6
-console.log(sumAllNumbers(1, 2, 3, 1, 2, 1, 10)) // 20
-console.log(sumAllNumbers(1, '2', 3)) // Error! You can use only numbers.
-console.log(sumAllNumbers([1, 2])) // Error! You can use only numbers.
-console.log(sumAllNumbers({ number: 1 })) // Error! You can use only numbers.
+renderUser(userData);
 
