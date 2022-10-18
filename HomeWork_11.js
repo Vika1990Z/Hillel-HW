fe0f0e2fc1f8bdb1df776c6b8c7e2db8644ffdb0
Vika1990Z <53933994+Vika1990Z@users.noreply.github.com>
2022-10-18 22:16:28 +0300

    fix

--- HomeWork_11.js
+++ HomeWork_11.js
@@ -1,13 +1,46 @@
+//___________________task form HILLEL________________________
 // Напишите функцию remove_char(str), которая возвращает строку, очищенную от всех не буквенно-цифровых символов
 
-function removeChar (str) {
+// function removeChar (str) {
+//     let resultStr = ''
+//     for (let i = 0; i < str.length; i++) {
+//         if (str[i].toUpperCase() != str[i].toLowerCase()) resultStr += str[i];    
+//     }
+//     return resultStr;
+// }
+
+// let str = prompt("enter some string");
+// console.log(`INFO: entered string "${str}"`);
+// console.log(`RESULT: ${removeChar(str)}`);
+
+
+
+//___________________ fixed ________________________
+const removeChar = str => {
     let resultStr = ''
-    for (let i = 0; i < str.length; i++) {
-        if (str[i].toUpperCase() != str[i].toLowerCase()) resultStr += str[i];    
-    }
+        for (let i = 0; i < str.length; i++) {
+            if (str[i].toUpperCase() != str[i].toLowerCase() || !Number.isNaN(+str[i]) && str[i] !=" ") resultStr += str[i];    
+        }
+    console.log(resultStr)    
     return resultStr;
 }
 
-let str = prompt("enter some string");
-console.log(`INFO: entered string "${str}"`);
-console.log(`RESULT: ${removeChar(str)}`);
+removeChar('qjwef b4385 **');
+
+
+
+// or
+const removeChar2 = str => {
+    return Array.from(str).reduce((acc, curr) => {
+        if (curr.toLowerCase() != curr.toUpperCase() || !Number.isNaN(+curr) && curr != ' ') acc += curr;
+        return acc;
+    }, "");
+}
+console.log(removeChar2('askndf4**.  *398'));
+
+
+
+// or with RegExp
+const removeChar3 = str => str.replace(/\W/g,'');
+
+console.log(removeChar3('askndf4**.  *398'));
