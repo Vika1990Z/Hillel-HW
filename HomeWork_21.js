08e044f1a115116c6a048c52ee3fc7005c6db01f
Vika1990Z <53933994+Vika1990Z@users.noreply.github.com>
2022-10-19 15:27:05 +0300

    fix

--- HomeWork_21.js
+++ HomeWork_21.js
@@ -1,11 +1,29 @@
+//___________________task form HILLEL________________________
 // Напишите функцию, которая делает поверхностную проверку объекта на пустоту.
 
-const isEmpty = function(object){
-    return Object.values(object).filter(el => !!el).length===0;
-  }
+// const isEmpty = function(object){
+//     return Object.values(object).filter(el => !!el).length===0;
+//   }
   
+// const data = { a: 1, b: undefined };
+// const data2 = { a: undefined };
+// console.log(isEmpty(data)); // false
+// console.log(isEmpty(data2)); // true
+
+
+
+
+//___________________updated task________________________
+// написати функцію, яка перевіряє чи в обʼєкті якісь властивості взагалі, 
+// цикли та ітеруючі методи не використовувати
+
+
+const isEmpty = object => Object.getOwnPropertyNames(object).length != 0;
+
+
 const data = { a: 1, b: undefined };
 const data2 = { a: undefined };
+const data3 = {};
 console.log(isEmpty(data)); // false
 console.log(isEmpty(data2)); // true
-
+console.log(isEmpty(data3)); // true
