f34b92acc14162c6b1243bc0fd1a01d83a0d1e66
Vika1990Z <53933994+Vika1990Z@users.noreply.github.com>
2022-10-19 15:28:39 +0300

    fix

--- HomeWork_22.js
+++ HomeWork_22.js
@@ -1,15 +1,33 @@
+//___________________task form HILLEL________________________
+// Напишите функцию, которая поверхностно сравнивает два объекта.
+
+// const isEqual = function(firstObject, secondObject){
+//     if (Object.keys(firstObject).length !== Object.keys(secondObject).length) return false;
+//     else {
+//         for (let i=0; i<Object.keys(firstObject).length; i++) {
+//             if (Object.keys(firstObject)[i] !== Object.keys(secondObject)[i] || Object.values(firstObject)[i] !== Object.values(secondObject)[i]) return false;
+//         }
+//     }
+//     return true;
+
+// } 
+
+  
+// const data = { a: 1, b: 1 };
+// const data2 = { a: 1, b: 1 };
+// const data3 = { a: 1, b: 2 };
+// const data4 = { a: 1, c: 1 };
+// const data5 = { a: 1, b: 1, c: 1 };
+// console.log(isEqual(data, data2)); // true
+// console.log(isEqual(data, data3)); // false
+// console.log(isEqual(data, data4)); // false
+// console.log(isEqual(data, data5)); // false
 
 
-const isEqual = function(firstObject, secondObject){
-    if (Object.keys(firstObject).length !== Object.keys(secondObject).length) return false;
-    else {
-        for (let i=0; i<Object.keys(firstObject).length; i++) {
-            if (Object.keys(firstObject)[i] !== Object.keys(secondObject)[i] || Object.values(firstObject)[i] !== Object.values(secondObject)[i]) return false;
-        }
-    }
-    return true;
+//___________________updated task________________________
+// написати функцію, яка перевіряє чи рівні обʼєкти за значенням; обʼєкти можуть мати вкладені обʼекти
 
-} 
+const isEqual = (firstObj, secondObj) => JSON.stringify(firstObj) === JSON.stringify(secondObj)
 
   
 const data = { a: 1, b: 1 };
@@ -17,7 +35,16 @@ const data2 = { a: 1, b: 1 };
 const data3 = { a: 1, b: 2 };
 const data4 = { a: 1, c: 1 };
 const data5 = { a: 1, b: 1, c: 1 };
+const data6 = { a: 1, b: 1, c: {
+    a: 1,
+    b: 2,
+} };
+const data7 = { a: 1, b: 1, c: {
+    a: 1,
+    b: 1,
+} };
 console.log(isEqual(data, data2)); // true
 console.log(isEqual(data, data3)); // false
 console.log(isEqual(data, data4)); // false
 console.log(isEqual(data, data5)); // false
+console.log(isEqual(data6, data7)); // false
