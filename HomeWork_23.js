e0856f4f4506da109faeca5d5e1ef6501b48b79a
Vika1990Z <53933994+Vika1990Z@users.noreply.github.com>
2022-10-19 15:29:50 +0300

    fix

--- HomeWork_23.js
+++ HomeWork_23.js
@@ -1,12 +1,23 @@
+//___________________task form HILLEL________________________
 // Напишите функцию, которая возвращает вложенный массив вида [[key, value], [key, value]].
 
-const makePairs = function(object){
-    let result = [];
-    for(let i=0; i < Object.keys(object).length; i++) {
-        result.push([Object.keys(object)[i], Object.values(object)[i]]);
-    }
-    return result;
-}
-const data = { a: 1, b: 2 };
-console.log(makePairs(data)); // [['a', 1], ['b', 2]]
+// const makePairs1 = function(object){
+//     let result = [];
+//     for(let i=0; i < Object.keys(object).length; i++) {
+//         result.push([Object.keys(object)[i], Object.values(object)[i]]);
+//     }
+//     return result;
+// }
+// const data1 = { a: 1, b: 2 };
+// console.log(makePairs1(data1)); // [['a', 1], ['b', 2]]
      
+
+
+
+
+//___________________updated task________________________
+// переписати не використовуючи методи Object.keys() та Object.values()
+const makePairs = obj => Object.entries(obj);
+
+const data = { a: 1, b: 2 };
+console.log(makePairs(data));
