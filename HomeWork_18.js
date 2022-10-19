5819487e5e566b13217072e75aa405aebe3a7f53
Vika1990Z <53933994+Vika1990Z@users.noreply.github.com>
2022-10-19 15:25:02 +0300

    fix

--- HomeWork_18.js
+++ HomeWork_18.js
@@ -1,24 +1,44 @@
-// Палиндромом называют слово или фразу, которая читается в обе стороны одинаково. В этом задании нужно написать функцию, которая проверяет, является ли исходная строка палиндромом.
-function removeSpaces(str){
-    let resulrStr = '';
-    for (let i=0; i<str.length; i++) {
-        if (str[i] !== " ") resulrStr += str[i];    
-    }
-    return resulrStr;
+//___________________task form HILLEL________________________
+// Палиндромом называют слово или фразу, которая читается в обе стороны
+// одинаково. 
+//В этом задании нужно написать функцию, которая проверяет, является ли исходная строка палиндромом.
+// function removeSpaces(str){
+//     let resulrStr = '';
+//     for (let i=0; i<str.length; i++) {
+//         if (str[i] !== " ") resulrStr += str[i];    
+//     }
+//     return resulrStr;
+//   }
+
+// function palindrome(str) {
+//     let initialStr = removeSpaces(str);
+//     let initialArr = initialStr.split('');
+//     let reverseArr = initialArr.slice().reverse();
+//     let reverseStr = reverseArr.join('');
+//     return initialStr.toLowerCase() === reverseStr.toLowerCase();
+//   }
+
+// console.log(palindrome('racecar')); // true
+// console.log(palindrome('table')); // false
+// console.log(palindrome('Анна')); // true
+// console.log(palindrome('А роза упала на лапу Азора')); // true
+
+
+
+
+
+//___________________updated task________________________
+// переписати використовуючи тільки цикл for з декількома умовами
+
+
+const palindrome = str => {
+  for (let i=0, j=str.length-1; i<str.length, j>=0; i++, j--) {
+    if (str[i].toLowerCase() === str[j].toLowerCase()) return true;
+    else return false;
   }
+}
 
-function palindrome(str) {
-    let initialStr = removeSpaces(str);
-    let initialArr = initialStr.split('');
-    let reverseArr = initialArr.slice().reverse();
-    let reverseStr = reverseArr.join('');
-    return initialStr.toLowerCase() === reverseStr.toLowerCase();
-  }
-  
-
-
-
-  console.log(palindrome('racecar')); // true
-  console.log(palindrome('table')); // false
-  console.log(palindrome('Анна')); // true
-  console.log(palindrome('А роза упала на лапу Азора')); // true
+console.log(palindrome('racecar')); // true
+console.log(palindrome('table')); // false
+console.log(palindrome('Анна')); // true
+console.log(palindrome('А роза упала на лапу Азора')); // true
